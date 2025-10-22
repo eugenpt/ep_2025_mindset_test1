87776b49ffe825196bd1523842b204578b121cba
ep <eugen.pt@gmail.com>
2025-10-22 21:22:55 +0700

    setup, init

---
+++ geometry_lib/__init__.py
@@ -0,0 +1,16 @@
+
+from .shapes import (
+    Shape,
+    Circle,
+    Triangle,
+    calc_area
+)
+
+__version__ = "1.0.0"
+__author__ = "ep"
+__all__ = [
+    "Shape",
+    "Circle",
+    "Triangle",
+    "calculate_area"
+]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geometry-lib",
+    version="1.0.0",
+    description="Библиотека для вычисления площадей геометрических фигур",
+    author="ep",
+    author_email="eugen.pt@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
